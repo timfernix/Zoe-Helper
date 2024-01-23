a7af2b82184238195b8f09a3b7d2f38616f5a339
Tim <46400905+timfernix@users.noreply.github.com>
2024-01-23 21:35:54 +0100

    Add files via upload

---
+++ src/textStorage.js
@@ -0,0 +1,34 @@
+const textStorage = {
+  supportTitle: "Zoe Support | Post Review - Welcome!",
+  feedbackTitle: "Zoe Feedback | Post Review - Welcome!",
+
+  supportDesc: "\n Here you can review and check the details of your post while we work on it. We will notify you as soon as we can when we reply to you in person. \n\n *Please apologise for any possible waiting time.*\n\nYou can fix simple problems here: https://discord.com/channels/554578876811182082/583897341392584715\nCurrent problems are addressed here: https://discord.com/channels/554578876811182082/554579908270227456\nYou can also take a look at the FAQ: [FAQ](https://wiki.zoe-discord-bot.ch/en/faq)",
+  feedbackDesc: "\n Here you can review and check the details of your post while we work on it. We will notify you as soon as we can when we reply to you in person. \n\n *Please apologise for any possible waiting time.*\n\n If you are interested in what we have planned for the future so far, you can take a look here: [Development Status](https://wiki.zoe-discord-bot.ch/en/Development-Status)",
+  aboutDesc: "**Zoe Helper** is a small bot that only takes care of some tasks on the Support and Development server of [Zoe the League of Legends Discord Bot](https://discord.com/oauth2/authorize?client_id=550737379460382752&scope=bot&permissions=3490049104&guild_id=0&response_type=code&redirect_uri=https%3A%2F%2Fzoe-discord-bot.ch%2FThanksYou.html).\n\n **Author:** <@589773984447463434>\n **Language:** `JavaScript`",
+  
+  footerText: "Zoe Helper by @timfernix | This is an automatically generated message.",
+  footerURL: "https://cdn.discordapp.com/avatars/589773984447463434/30bd767959243b31892613dcc56ff6c0.png?size=64",
+  thumbnailLink: "https://cdn.discordapp.com/attachments/1179434091964285042/1198376353679020203/Zoe_1.jpg?ex=65beadf3&is=65ac38f3&hm=f659dab70ea493296c01da4e9cafb9cb5dbd909e80cb279cce0878636e143487&",
+  
+  infoInfochannel: "The infochannel contains the infopanel and the gamecards. These are deleted after the end of the game (including all other messages in the channel). This leads to problems if you define other channels in this channel.",
+  infoInfochannelWiki: "- [Infochannel](https://wiki.zoe-discord-bot.ch/en/features/infoChannel)\n- [Gamecards](https://wiki.zoe-discord-bot.ch/en/features/gamecards)\n- [Infochannel Order](https://wiki.zoe-discord-bot.ch/en/Zoe-Configuration/Infochannel/Infochannel-Order)\n- [Infochannel Rankfilter](https://wiki.zoe-discord-bot.ch/en/Zoe-Configuration/Infochannel/Infochannel-Rankfilter)",
+  infoRankchannel: "Messages about LP wins and losses of players are sent in the rankchannel. If they take some time to be sent, it may be because you have not switched on your discord presence. Placement games are not displayed in the ranked channel.",
+  infoRankchannelWiki: "- [Rankchannel](https://wiki.zoe-discord-bot.ch/en/features/rankChannel)\n- [Rankchannel Filter](https://wiki.zoe-discord-bot.ch/en/Zoe-Configuration/Rankchannel/Rankchannel-Filter)",
+  infoLeaderboards: "In leaderboards, players can compare themselves in different categories. Mastery points / KDA from different accounts are not offset against each other. For ranks, the highest is always displayed. Leaderboards that are too high up in a channel (> 20 messages) cannot be kept up to date by Zoe.",
+  infoLeaderboardsWiki: "- [Leaderboards](https://wiki.zoe-discord-bot.ch/en/features/leaderboards)",
+  infoClashchannel: "In the clashchannel you can get tips on picks and bans in clash. For the clashchannel to show a team you have to be in a team with 4 other people.",
+  infoClashchannelWiki: "- [Clashchannel](https://wiki.zoe-discord-bot.ch/en/features/clashChannel)",
+  infoMatchhistorychannel: "The matchhistorychannel is an advanced rankchannel that does not show LP changes but the exact match details.",
+  infoMatchhistorychannelWiki: "- [Matchhistorychannel](https://wiki.zoe-discord-bot.ch/en/features/matchhistoryChannel)",
+  infoRankroles: "Rankroles show the current rank of your players in their Discord profile. You need enough space on your server to set them. With the premium version you can customise them in the smallest detail. They are updated normally like the other features.",
+  infoRankrolesWiki: "- [Rankroles](https://wiki.zoe-discord-bot.ch/en/features/rankroles)",
+  infoSubscription: "If you want to support us, you can do so with a subscription and receive some benefits in return. If you cant use your benefits, this may be due to the following: \n- You have not connected your Discord account to Kofi. \n- Your subscription has expired or has not been automatically renewed. \n- You have not used the /boost command on your server. \n Please note that you must stay on this server to keep your benefits.",
+  infoSubscriptionWiki: "- [Support](https://wiki.zoe-discord-bot.ch/en/support)\n- [Zoe Points](https://wiki.zoe-discord-bot.ch/en/Zoe-Points-And-Boosting)\n- [/boost](https://wiki.zoe-discord-bot.ch/en/commands/important/boost)\n- [Guide](https://wiki.zoe-discord-bot.ch/en/Guides/Subscription)",
+  infoRefresh: "Your features are not updating? \n If this is the case and occurs acutely, please check in https://discord.com/channels/554578876811182082/554579908270227456 whether a message has already been issued there. If there is nothing there, thank you for your report! \n If it has not been working for some time, please check https://discord.com/channels/554578876811182082/583897341392584715",
+  infoRefreshWiki: "- [Refresh](https://wiki.zoe-discord-bot.ch/en/terms/refresh-mode)\n- [/refresh](https://wiki.zoe-discord-bot.ch/en/commands/important/refresh)",
+  infoError: "You have come across an error, thank you for reporting it, if it is not an permission error it could be something more serious. Thank you for your report.",
+  infoConfig: "Zoe can be customised according to your wishes using the /config command.",
+  infoConfigWiki: "- [Configuration](https://wiki.zoe-discord-bot.ch/en/Zoe-Configuration)",
+};
+  
+  module.exports = textStorage;
