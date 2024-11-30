0ee6138bfa0b0e3904d4f7bd083a6a49af858227
Tim <46400905+timfernix@users.noreply.github.com>
2024-11-30 11:31:41 +0100

    Delete src directory

--- src/events/ready/consoleLog.js
+++
@@ -1,3 +0,0 @@
-module.exports = (c, client, handler) => {
-    console.log(`✅ ${client.user.tag} is online.`);
-  };
--- src/events/ready/updatePresence.js
+++
@@ -1,9 +0,0 @@
-const {ActivityType} = require('discord.js')
-
-module.exports = (c, client, handler) => {
-    client.user.setActivity({
-        name: 'over the Server',
-        type: ActivityType.Watching,
-      })
-  };
-
